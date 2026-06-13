179ac5440bd6ff72fef70fe809d4510354c1108b
Edmond Vardanyan <edmondvar2005@gmail.com>
2026-06-13 18:39:23 +0400

    ex4

--- ex4/ft_finally_block.py
+++ ex4/ft_finally_block.py
@@ -1,1 +1,50 @@
-...
+#!/usr/bin/env python3
+
+class GardenError(Exception):
+    def __init__(self, message: str = "Unknown garden error") -> None:
+        Exception.__init__(self, message)
+
+
+class PlantError(GardenError):
+    def __init__(self, message: str = "Unknown plant error") -> None:
+        GardenError.__init__(self, message)
+
+
+def water_plant(plant_name: str) -> None:
+    if plant_name != plant_name.capitalize():
+        raise PlantError(f"Invalid plant name to water: '{plant_name}'")
+    print(f"Watering {plant_name}: [OK]")
+
+
+def test_watering_system() -> None:
+    print("=== Garden Watering System ===")
+    try:
+        print("Testing valid plants...")
+        print("Opening watering system")
+        water_plant("Tomato")
+        water_plant("Lettuce")
+        water_plant("Carrots")
+    except PlantError as error:
+        print(f"Caught PlantError: {error}")
+        print(".. ending tests and returning to main")
+        return
+    finally:
+        print("Closing watering system")
+
+    try:
+        print("Testing invalid plants...")
+        print("Opening watering system")
+        water_plant("Tomato")
+        water_plant("lettuce")
+        water_plant("carrots")
+    except PlantError as error:
+        print(f"Caught PlantError: {error}")
+        print(".. ending tests and returning to main")
+        return
+    finally:
+        print("Closing watering system")
+
+
+if __name__ == "__main__":
+    test_watering_system()
+    print("Cleanup always happens, even with errors!")
